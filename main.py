7c1f837687ded9144e04883d638cb0d4538ea236
FrancoVarela10 <95846061+FrancoVarela10@users.noreply.github.com>
2024-10-10 23:11:53 -0600

    instructions

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import csv
 #import random
 import random
 
+#WELCOME MESSAGE ===============>
+print('======== WELCOME TO OUR COGNITIVE GAME ============')
+
 #OPEN THE CSV
 with open('RANDOM_WORDS - Hoja 1.csv', mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
@@ -28,3 +31,35 @@ if lang == 1:
 elif lang == 2:
     random_english = random_word['ENGLISH']
     print(random_english)
+
+
+def switch_case(lang):
+    if lang == 1:
+        print('======== REGLAS ============')
+        print(' ')
+        print('Empieza a escribir una palabra de 5 letras.')
+        print(' ')
+        print('Solo tendrás 5 intentos para adivinar la palabra correcta.')
+        print(' ')
+        print('Verde = letra y posición correcta')
+        print(' ')
+        print('Amarillo = letra correcta y posición incorrecta')
+        print(' ')
+        print('Rojo = letra incorrecta y posición incorrecta')
+    elif lang == 2:
+        print('======== RULES ============')
+        print(' ')
+        print('Start typing a word of 5 letters.')
+        print(' ')
+        print('You will have only 5 attempts to guess the right word.')
+        print(' ')
+        print('Green = correct letter and correct position')
+        print(' ')
+        print('Yellow = correct letter but incorrect position')
+        print(' ')
+        print('Red = incorrect letter and incorrect position')
+    else:
+        print('Opción no válida. Por favor, elige 1 para español o 2 para inglés.')
+
+# PRINTING RULES
+switch_case(lang)
